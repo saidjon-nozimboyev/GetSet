3e6794ffcf848a921f8be79347e2b539a639385b
user <user@WIN-9TE5F9EN5LS>
2024-01-07 22:14:06 +0500

    Add project files.

---
+++ GetSet/Program.cs
@@ -0,0 +1,36 @@
+﻿class Student
+{
+
+    public int birthYear;
+    public int Age
+    {
+        get
+        {
+            return DateTime.Now.Year - birthYear;
+        }
+        set
+        {
+            if (value <= DateTime.Now.Year - birthYear && value > 0)
+            {
+                birthYear = DateTime.Now.Year - birthYear - value;
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student = new Student();
+        Console.Write("Please enter age or year: ");
+        student.Age = int.Parse(Console.ReadLine()!);
+        Console.WriteLine(student.birthYear);
+        Console.WriteLine(student.Age);
+    }
+
+}
